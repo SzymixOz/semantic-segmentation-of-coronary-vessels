d09d729314ae81cf005336cf0b77d6d644a0b6bc
Wiktor Satora <wiktor.satora@gmail.com>
2024-06-12 00:11:02 +0200

    Image to Cv2 change

--- prototype/prototype.py
+++ prototype/prototype.py
@@ -1,5 +1,6 @@
 import tkinter as tk
 from tkinter import filedialog, ttk
+import cv2
 from PIL import Image, ImageTk
 
 loaded_image_tab1 = None
@@ -12,17 +13,19 @@ def upload_image_tab1():
     global loaded_image_tab1, photo1_tab1
     file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.gif")])
     if file_path:
-        loaded_image_tab1 = Image.open(file_path)
-        resized_image = loaded_image_tab1.resize((250, 250))
-        photo1_tab1 = ImageTk.PhotoImage(resized_image)
+        loaded_image_tab1 = cv2.imread(file_path)
+        resized_image = cv2.resize(loaded_image_tab1, (250, 250))
+        resized_image_rgb = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
+        photo1_tab1 = ImageTk.PhotoImage(Image.fromarray(resized_image_rgb))
         image_label1_tab1.config(image=photo1_tab1)
         image_label1_tab1.image = photo1_tab1
 
 def predict_image_tab1():
     global loaded_image_tab1
-    if loaded_image_tab1:
-        resized_image = loaded_image_tab1.resize((250, 250))
-        photo2_tab1 = ImageTk.PhotoImage(resized_image)
+    if loaded_image_tab1 is not None:
+        resized_image = cv2.resize(loaded_image_tab1, (250, 250))
+        resized_image_rgb = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
+        photo2_tab1 = ImageTk.PhotoImage(Image.fromarray(resized_image_rgb))
         image_label2_tab1.config(image=photo2_tab1)
         image_label2_tab1.image = photo2_tab1
 
@@ -30,17 +33,19 @@ def upload_image_tab2():
     global loaded_image_tab2, photo1_tab2
     file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.gif")])
     if file_path:
-        loaded_image_tab2 = Image.open(file_path)
-        resized_image = loaded_image_tab2.resize((250, 250))
-        photo1_tab2 = ImageTk.PhotoImage(resized_image)
+        loaded_image_tab2 = cv2.imread(file_path)
+        resized_image = cv2.resize(loaded_image_tab2, (250, 250))
+        resized_image_rgb = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
+        photo1_tab2 = ImageTk.PhotoImage(Image.fromarray(resized_image_rgb))
         image_label1_tab2.config(image=photo1_tab2)
         image_label1_tab2.image = photo1_tab2
 
-def proceed2():
+def predict_image_tab2():
     global loaded_image_tab2
-    if loaded_image_tab2:
-        resized_image = loaded_image_tab2.resize((250, 250))
-        photo2_tab2 = ImageTk.PhotoImage(resized_image)
+    if loaded_image_tab2 is not None:
+        resized_image = cv2.resize(loaded_image_tab2, (250, 250))
+        resized_image_rgb = cv2.cvtColor(resized_image, cv2.COLOR_BGR2RGB)
+        photo2_tab2 = ImageTk.PhotoImage(Image.fromarray(resized_image_rgb))
         image_label2_tab2.config(image=photo2_tab2)
         image_label2_tab2.image = photo2_tab2
 
@@ -74,7 +79,7 @@ notebook.add(tab2, text='DICOM image prediction')
 upload_button_tab2 = tk.Button(tab2, text="Upload Image", command=upload_image_tab2)
 upload_button_tab2.grid(row=0, column=0, padx=10, pady=10)
 
-proceed_button_tab2 = tk.Button(tab2, text="Predict", command=proceed2)
+proceed_button_tab2 = tk.Button(tab2, text="Predict", command=predict_image_tab2)
 proceed_button_tab2.grid(row=0, column=1, padx=10, pady=10)
 
 image_label1_tab2 = tk.Label(tab2, image=empty_image)
