7625795d21509a1aca30659aec2b68f53e4a3852
Szymon Ożóg <szymon.ozog@onet.eu>
2024-10-19 21:04:57 +0200

    Several version of left/right model and stage 3 model

---
+++ nn_1_stage_3.0_version/large_RGB_model.py
@@ -0,0 +1,70 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class UNet(nn.Module):
+    def __init__(self, num_classes=28, dropout_rate=0.2):
+        super(UNet, self).__init__()
+        
+        def CBR(in_channels, out_channels, dropout_rate):
+            return nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+                nn.BatchNorm2d(out_channels),
+                nn.ReLU(inplace=True),
+                nn.Dropout2d(dropout_rate)
+            )
+
+        self.enc1 = CBR(3, 64, dropout_rate)
+        self.enc2 = CBR(64, 128, dropout_rate)
+        self.enc3 = CBR(128, 256, dropout_rate)
+        self.enc4 = CBR(256, 512, dropout_rate)
+        
+        self.pool = nn.MaxPool2d(2)
+
+        self.bottleneck = CBR(512, 1024, dropout_rate)
+
+        self.upconv4 = nn.ConvTranspose2d(1024, 512, kernel_size=2, stride=2)
+        self.dec4 = CBR(1024, 512, dropout_rate)
+        self.upconv3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.dec3 = CBR(512, 256, dropout_rate)
+        self.upconv2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.dec2 = CBR(256, 128, dropout_rate)
+        self.upconv1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+        self.dec1 = CBR(128, 64, dropout_rate)
+        
+        self.conv_last = nn.Conv2d(64, num_classes, kernel_size=1)
+
+    def forward(self, x):
+        enc1 = self.enc1(x)
+        enc2 = self.enc2(self.pool(enc1))
+        enc3 = self.enc3(self.pool(enc2))
+        enc4 = self.enc4(self.pool(enc3))
+        
+        bottleneck = self.bottleneck(self.pool(enc4))
+
+        dec4 = self.upconv4(bottleneck)
+        dec4 = torch.cat((dec4, enc4), dim=1)
+        dec4 = self.dec4(dec4)
+        
+        dec3 = self.upconv3(dec4)
+        dec3 = torch.cat((dec3, enc3), dim=1)
+        dec3 = self.dec3(dec3)
+        
+        dec2 = self.upconv2(dec3)
+        dec2 = torch.cat((dec2, enc2), dim=1)
+        dec2 = self.dec2(dec2)
+        
+        dec1 = self.upconv1(dec2)
+        dec1 = torch.cat((dec1, enc1), dim=1)
+        dec1 = self.dec1(dec1)
+        
+        x = self.conv_last(dec1)
+        return x  # Zwrot logitów
+
+    def predict(self, x):
+        logits = self.forward(x)
+        probs = F.softmax(logits, dim=1)
+        return torch.argmax(probs, dim=1)  # Zwrot etykiet klas (wartości całkowitoliczbowe)
---
+++ nn_2_stage_3.0_version/skrypt_left.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -l
+#SBATCH -J SEGNW-test
+## Liczba alokowanych węzłów
+#SBATCH -N 1
+## Liczba zadań per węzeł (domyślnie jest to liczba alokowanych rdzeni na węźle)
+#SBATCH --ntasks-per-node=1
+## Ilość pamięci przypadającej na jeden rdzeń obliczeniowy (domyślnie 5GB na rdzeń)
+#SBATCH --mem-per-cpu=16GB
+## Maksymalny czas trwania zlecenia (format HH:MM:SS)
+#SBATCH --time=00:25:00 
+## Nazwa grantu do rozliczenia zużycia zasobów CPU
+#SBATCH -A plgsegnw-gpu-a100
+## Specyfikacja partycji
+#SBATCH -p plgrid-gpu-a100
+## Liczba GPU
+#SBATCH --gres=gpu:1
+## Plik ze standardowym wyjściem
+#SBATCH --output="output_time_left.out"
+## Plik ze standardowym wyjściem błędów
+#SBATCH --error="error_left.err"
+
+## przejscie do katalogu z ktorego wywolany zostal sbatch
+cd $SLURM_SUBMIT_DIR
+
+srun python train_model_left.py
