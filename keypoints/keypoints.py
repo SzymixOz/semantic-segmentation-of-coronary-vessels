ad324e7eaf10e449935eea1c59a41d7294e3deff
Thyen-Radek <rabuhrabuhrabuh@poczta.fm>
2024-10-30 07:50:43 +0100

    Added connected crossings

--- keypoints/keypoints.py
+++ keypoints/keypoints.py
@@ -88,6 +88,7 @@ def find_keypoints_on_graph(skeleton: np.ndarray) -> Tuple[np.ndarray, list, lis
         - crossings (list): A list of indices representing crossing points in the skeleton.
         - bifurcations (list): A list of indices representing bifurcation points in the skeleton.
         - filtered_out (list): A list of indices representing filtered out points in the skeleton.
+        - crossings_connected (list): A list of tuples representing connected crossings in the skeleton.
     """
 
 
@@ -134,9 +135,30 @@ def find_keypoints_on_graph(skeleton: np.ndarray) -> Tuple[np.ndarray, list, lis
                         new_depth = depth + 1 if next in filtered_branchpoints else depth # (this is for not checking filtered_out)
                         if new_depth < 2:
                             queue.append((next, path + [next], new_depth))
-    crossings = []
-    # filtered_branchpoints = [branchpoint for branchpoint in filtered_branchpoints if branchpoint not in crossings] doubt if this is needed
 
+    # define bfs that will check for that one neighbor and find nearest branchpoint
+    def bfs_find_in_distance(graph, start, branchpoints, max_distance):
+        queue = [(start, [start], 0)]
+        visited = set()
+        for branchpoint in branchpoints:
+            visited.add(branchpoint)
+        while queue:
+            (vertex, path, distance) = queue.pop(0)
+            visited.add(vertex)
+            for next in graph[vertex]:
+                if next not in visited:
+                    new_distance = distance + 1
+                    if next in filtered_branchpoints:
+                        return next
+                    if new_distance < max_distance:
+                        queue.append((next, path + [next], new_distance))
+                    else:
+                        return None
+    
+    crossings = []
+    crossings_connected = []
+    # filtered_branchpoints = [branchpoint for branchpoint in filtered_branchpoints if branchpoint not in crossings] doubt if this is needed
+    
     for i, branchpoint1 in enumerate(filtered_branchpoints):
         for branchpoint2 in filtered_branchpoints[i+1:]:
             paths = []
@@ -146,13 +168,32 @@ def find_keypoints_on_graph(skeleton: np.ndarray) -> Tuple[np.ndarray, list, lis
                 paths.extend(new_path for new_path in new_paths if not any(set(new_path[1:-1]).intersection(set(existing_path[1:-1])) for existing_path in paths))
       
             if len(paths) > 1:
-                crossings.append(branchpoint2)
+                neighbors = list(G.neighbors(branchpoint2))
+                neighbors_filtered_out = []
+                for neighbor in neighbors:
+                    for path in paths:
+                        if neighbor in path:
+                            neighbors_filtered_out.append(neighbor)
+                            break
+                neighbors = [neighbor for neighbor in neighbors if neighbor not in neighbors_filtered_out]
+                if len(neighbors) > 1:
+                    crossings.append(branchpoint2)
+                    # Not sure if that ever will happen
+                else:
+                    max_distance = 50
+                    nearest_branchpoint = bfs_find_in_distance(G, branchpoint2, neighbors_filtered_out, max_distance)
+                    if nearest_branchpoint is not None:
+                        crossings_connected.append((branchpoint2, nearest_branchpoint))
+
+    print(f"Possible: {crossings_connected}")
 
     filtered_branchpoints = [branchpoint for branchpoint in filtered_branchpoints if branchpoint not in crossings]
+    filtered_branchpoints = [branchpoint for branchpoint in filtered_branchpoints if branchpoint not in [branchpoint for branchpoint, _ in crossings_connected]]
+    filtered_branchpoints = [branchpoint for branchpoint in filtered_branchpoints if branchpoint not in [branchpoint for _, branchpoint in crossings_connected]]
     
-    return coordinates, endpoints, crossings, filtered_branchpoints, filtered_out
+    return coordinates, endpoints, crossings, filtered_branchpoints, filtered_out, crossings_connected
 
-def draw_everything_else(image_binary: np.ndarray, skeleton: np.ndarray, coordinates: Tuple[np.ndarray, np.ndarray], endpoints: list, crossings: list, filtered_branchpoints: list, filtered_out: list) -> None:
+def draw_everything_else(image_binary: np.ndarray, skeleton: np.ndarray, coordinates: Tuple[np.ndarray, np.ndarray], endpoints: list, crossings: list, filtered_branchpoints: list, filtered_out: list, crossings_connected: list) -> None:
     """
         This function takes a binary image, a skeletonized image, and the keypoints of the skeleton and displays the images side by side.
 
@@ -210,11 +251,19 @@ def draw_everything_else(image_binary: np.ndarray, skeleton: np.ndarray, coordin
     for branchpoint in crossings:
         # candidates for crossroads
         ax3.scatter(coordinates[1][branchpoint], coordinates[0][branchpoint], c='r', s=size)
+        
+    for branchpoint1, branchpoint2 in crossings_connected:
 
+        ax3.scatter(coordinates[1][branchpoint1], coordinates[0][branchpoint1], c='r', s=size, zorder=5)
+        ax3.scatter(coordinates[1][branchpoint2], coordinates[0][branchpoint2], c='r', s=size, zorder=5)
+
+        ax3.plot([coordinates[1][branchpoint1], coordinates[1][branchpoint2]], [coordinates[0][branchpoint1], coordinates[0][branchpoint2]], c='orange', linewidth=4)
+        
     # legend blue - bifurcation, red potential crossing, green - filtered out
     legend_elements = [Line2D([0], [0], marker='o', color='w', label='Bifurcation', markerfacecolor='b', markersize=15),
                    Line2D([0], [0], marker='o', color='w', label='Filtered Out', markerfacecolor='g', markersize=15),
-                   Line2D([0], [0], marker='o', color='w', label='Potential Crossing', markerfacecolor='r', markersize=15)]
+                   Line2D([0], [0], marker='o', color='w', label='Potential Crossing', markerfacecolor='r', markersize=15),
+                     Line2D([0], [0], color='orange', linewidth=4, label='Connected Crossings')]
     ax3.legend(handles=legend_elements, loc='upper right', prop={'size': 15})
 
     # plt.subplots_adjust(wspace=0.05, hspace=0.10)
@@ -277,10 +326,10 @@ def keypoints(image_name: str, mask: np.ndarray, with_plot: bool = True) -> None
 
     skeleton = get_skeleton_from_mask(mask_preprocessed)
 
-    coordinates, endpoints, crossings, filtered_branchpoints, filtered_out = find_keypoints_on_graph(skeleton)
+    coordinates, endpoints, crossings, filtered_branchpoints, filtered_out, crossings_connected = find_keypoints_on_graph(skeleton)
 
     if with_plot:
-        draw_everything_else(mask_preprocessed, skeleton, coordinates, endpoints, crossings, filtered_branchpoints, filtered_out)
+        draw_everything_else(mask_preprocessed, skeleton, coordinates, endpoints, crossings, filtered_branchpoints, filtered_out, crossings_connected)
 
     save_points_to_grayscale_image(image_name, skeleton, coordinates, filtered_branchpoints, crossings, endpoints)
 
@@ -316,16 +365,20 @@ def make_all_images():
     images_voting, segmentations_voting, filenames_voting, labels_voting = get_data(data, voting=True, images_path=(data_folder + "images"), labeling=True)
     
     print("GENERATING KEYPOINTS...")
-    std_output = sys.stdout
-    sys.stdout = open(os.devnull, 'w') 
+    # std_output = sys.stdout
+    # sys.stdout = open(os.devnull, 'w') 
 
     for index in range(len(images_voting)):
+        if index < 0: # Skip first 8 images
+            continue
+        if index > 0: # Skip all images after 8th
+            break
         mask = get_mask(images_voting[index], segmentations_voting[index], labels_voting[index], name=filenames_voting[index], folder_name='ground_truth', binary=True, ground_truth=True)
         mask_gray = cv2.cvtColor(mask, cv2.COLOR_BGR2GRAY)
         mask_binary = mask_gray > 0
-        keypoints(filenames_voting[index], mask_binary, with_plot=False)
+        keypoints(filenames_voting[index], mask_binary, with_plot=True)
 
-    sys.stdout = std_output
+    # sys.stdout = std_output
 
     print("GENERATION OF KEYPOINTS FINISHED")
 
