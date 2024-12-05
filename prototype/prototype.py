6f1fb0eaf1db3a1ccf4222483d438a50843e7094
Wiktor Satora <wiktor.satora@gmail.com>
2024-12-05 20:05:49 +0100

    one tab in product

--- prototype/prototype.py
+++ prototype/prototype.py
@@ -29,44 +29,18 @@ def predict_image_tab1():
     if loaded_image_tab1 is not None:
         resized_image = cv2.resize(loaded_image_tab1, (256, 256))
         resized_image_rgb = cv2.cvtColor(resized_image, cv2.COLOR_RGBA2RGB)
-        resized_image_rgb = predict(resized_image_rgb, 'binary')
+        resized_image_rgb = predict(resized_image_rgb)
         photo2_tab1 = ImageTk.PhotoImage(Image.fromarray(resized_image_rgb))
         image_label2_tab1.config(image=photo2_tab1)
         image_label2_tab1.image = photo2_tab1
 
-def upload_image_tab2():
-    global loaded_image_tab2, photo1_tab2
-    file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.gif")])
-    if file_path:
-        loaded_image_tab2 = cv2.imread(file_path)
-        resized_image = cv2.resize(loaded_image_tab2, (256, 256))
-        resized_image_rgb = cv2.cvtColor(resized_image, cv2.COLOR_BGRA2RGB)
-        photo1_tab2 = ImageTk.PhotoImage(Image.fromarray(resized_image_rgb))
-        image_label1_tab2.config(image=photo1_tab2)
-        image_label1_tab2.image = photo1_tab2
-
-def predict_image_tab2():
-    global loaded_image_tab2
-    if loaded_image_tab2 is not None:
-        resized_image = cv2.resize(loaded_image_tab2, (256, 256))
-        resized_image_rgb = cv2.cvtColor(resized_image, cv2.COLOR_RGBA2RGB)
-        resized_image_rgb = predict(resized_image_rgb, 'dicom')
-        photo2_tab2 = ImageTk.PhotoImage(Image.fromarray(resized_image_rgb))
-        image_label2_tab2.config(image=photo2_tab2)
-        image_label2_tab2.image = photo2_tab2
-
-def predict(img, type):
+def predict(img):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     model = UNet()
     model = model.to(device)
-    if type == 'binary':
-        model.load_state_dict(torch.load('large_RGB_model_binary.pth'))
-    else:
-        model.load_state_dict(torch.load('large_RGB_model_dicom.pth'))
-    model.eval()
+    model.load_state_dict(torch.load('large_RGB_model_binary.pth'))
 
-    print(img.shape)
-    print(img)
+    model.eval()
 
     img = transforms.ToTensor()(img).unsqueeze(0).to(device)
     with torch.no_grad():
@@ -82,16 +56,11 @@ def predict(img, type):
     return pred_image
 
 
-
 root = tk.Tk()
-root.title("Semantic Segmentation Prediction Model (prototype)")
+root.title("Module for automatic segmentation of coronary vessels")
 root.geometry("600x350")
 
-notebook = ttk.Notebook(root)
-notebook.pack(expand=1, fill='both')
-
-tab1 = ttk.Frame(notebook)
-notebook.add(tab1, text='binary image prediction')
+tab1 = ttk.Frame(root)
 
 upload_button_tab1 = tk.Button(tab1, text="Upload Image", command=upload_image_tab1)
 upload_button_tab1.grid(row=0, column=0, padx=10, pady=10)
@@ -107,19 +76,6 @@ image_label1_tab1.grid(row=1, column=0, padx=10, pady=10)
 image_label2_tab1 = tk.Label(tab1, image=empty_image)
 image_label2_tab1.grid(row=1, column=1, padx=10, pady=10)
 
-tab2 = ttk.Frame(notebook)
-notebook.add(tab2, text='DICOM image prediction')
-
-upload_button_tab2 = tk.Button(tab2, text="Upload Image", command=upload_image_tab2)
-upload_button_tab2.grid(row=0, column=0, padx=10, pady=10)
-
-proceed_button_tab2 = tk.Button(tab2, text="Predict", command=predict_image_tab2)
-proceed_button_tab2.grid(row=0, column=1, padx=10, pady=10)
-
-image_label1_tab2 = tk.Label(tab2, image=empty_image)
-image_label1_tab2.grid(row=1, column=0, padx=10, pady=10)
-
-image_label2_tab2 = tk.Label(tab2, image=empty_image)
-image_label2_tab2.grid(row=1, column=1, padx=10, pady=10)
+tab1.pack(expand=1, fill='both')
 
 root.mainloop()
