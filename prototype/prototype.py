e8893b404ee9d6e98347647ac8058c14a9d60ba8
Wiktor Satora <wiktor.satora@gmail.com>
2024-12-05 23:28:15 +0100

    side detection UI added

--- prototype/prototype.py
+++ prototype/prototype.py
@@ -13,6 +13,8 @@ photo1_tab1 = None
 loaded_image_tab2 = None
 photo1_tab2 = None
 
+recognized_side = None
+detected_side_str = "Detected side:"
 def upload_image_tab1():
     global loaded_image_tab1, photo1_tab1
     file_path = filedialog.askopenfilename(filetypes=[("Image Files", "*.png;*.jpg;*.jpeg;*.gif")])
@@ -23,6 +25,10 @@ def upload_image_tab1():
         photo1_tab1 = ImageTk.PhotoImage(Image.fromarray(resized_image_rgb))
         image_label1_tab1.config(image=photo1_tab1)
         image_label1_tab1.image = photo1_tab1
+        recognized_side = False
+        side_label.config(text=f"{detected_side_str} {'left' if not recognized_side else 'right'}")
+        predict_button_tab1.config(state=tk.NORMAL)
+        toggle_button.grid(row=2, column=1, padx=10, pady=10)  # Show the Change side button
 
 def predict_image_tab1():
     global loaded_image_tab1
@@ -55,17 +61,22 @@ def predict(img):
     
     return pred_image
 
+def toggle_side():
+    global recognized_side
+    recognized_side = not recognized_side
+    side_label.config(text=f"{detected_side_str} {'left' if not recognized_side else 'right'}")
+
 
 root = tk.Tk()
 root.title("Module for automatic segmentation of coronary vessels")
-root.geometry("600x350")
+root.geometry("600x400")
 
 tab1 = ttk.Frame(root)
 
 upload_button_tab1 = tk.Button(tab1, text="Upload Image", command=upload_image_tab1)
 upload_button_tab1.grid(row=0, column=0, padx=10, pady=10)
 
-predict_button_tab1 = tk.Button(tab1, text="Predict", command=predict_image_tab1)
+predict_button_tab1 = tk.Button(tab1, text="Predict", command=predict_image_tab1, state=tk.DISABLED)
 predict_button_tab1.grid(row=0, column=1, padx=10, pady=10)
 
 empty_image = ImageTk.PhotoImage(Image.new('RGB', (256, 256), (200, 200, 200)))
@@ -75,6 +86,12 @@ image_label1_tab1.grid(row=1, column=0, padx=10, pady=10)
 
 image_label2_tab1 = tk.Label(tab1, image=empty_image)
 image_label2_tab1.grid(row=1, column=1, padx=10, pady=10)
+
+side_label = tk.Label(tab1, text=f"")
+side_label.grid(row=2, column=0, padx=10, pady=10)
+
+toggle_button = tk.Button(tab1, text="Change side", command=toggle_side)
+toggle_button.grid_forget()
 
 tab1.pack(expand=1, fill='both')
 
